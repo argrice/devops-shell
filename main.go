1aadfbc0c67d614f4f85472b3d85f0ff10f1c407
argrice <108051550+argrice@users.noreply.github.com>
2024-11-04 16:52:22 -0500

    Update main.go
    Commented

--- main.go
+++ main.go
@@ -7,45 +7,49 @@ import (
     "os/user"
     "path/filepath"
     "strings"
-
     "github.com/peterh/liner"
 )
 
 var historyFile string
 
-// ErrExit is returned when the shell should exit.
+// ErrExit is returned when loop needs to exit
 var ErrExit = errors.New("exit")
 
 func main() {
-    currUser, err := user.Current()
+    // User shouldn't be changing. This will change the shell itself and cause errors
+    currUser, err := user.Current()    
     if err != nil {
         fmt.Fprintln(os.Stderr, "Error getting current user:", err)
         return
     }
 
-    historyFile = filepath.Join(currUser.HomeDir, ".ash_history.txt")
-
     line := liner.NewLiner()
     defer line.Close()
 
+    // Create input history file in user's home dir
+    historyFile = filepath.Join(currUser.HomeDir, ".dv_shell_history.txt")
+    
     history := loadHistory(historyFile)
     for _, cmd := range history {
         line.AppendHistory(cmd)
     }
 
     for {
+        // Pull system's hostname
         hostname, err := os.Hostname()
         if err != nil {
             fmt.Fprintln(os.Stderr, "Error getting hostname:", err)
             continue
         }
 
+        // Pull current working directory
         cwd, err := os.Getwd()
         if err != nil {
             fmt.Fprintln(os.Stderr, "Error getting current working directory:", err)
             continue
         }
 
+        // Generate prompt for input
         prompt := fmt.Sprintf("%s@%s %s > ", currUser.Username, hostname, cwd)
         input, err := line.Prompt(prompt)
         if err != nil {
@@ -63,9 +67,11 @@ func main() {
             continue
         }
 
+        // Add command to history file and line for in-memory caching of history
         line.AppendHistory(input)
         history = append(history, input)
 
+        // For custom command runparallel, see taskManager.go
         if strings.HasPrefix(input, "runparallel ") {
             commands := strings.Split(input[len("runparallel "):], ";")
             tm := NewTaskManager()
